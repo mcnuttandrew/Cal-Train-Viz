bf736f1b646ce6bc216cdaf00823003a6299e441
Andrew McNutt <mcnutt.andrew@gmail.com>
2014-11-24 23:31:00 -0800

    refactors to include a scale element across the visualization

--- app/assets/javascripts/vizscript.js
+++ app/assets/javascripts/vizscript.js
@@ -20,15 +20,12 @@
 	}
 	
 	var buildGraphic = function(ctx, stops, trains){
-		var w = 2000;
-		var h = 1000;
-		var margins = {"left": 100, "right": 30, "top": 30, "bottom": 50};	
-		// debugger;																
+		var w = 800;
+		var h = 400;
 		var svg = d3.select(".d3canvas")
 								.append("svg")
 								.attr({"width": w, "height": h})
 								.append("g");
-								// .attr("transform", 'translate('+ w/2+ ','+ h/2 + ')');
 		buildGrid(svg, h, w, stops);			
 		buildTrainRoutes(svg, h, w, trains, stops);	
 
@@ -36,9 +33,12 @@
 	}
 	
 	var buildTrainRoutes = function(svg, h, w, trains, stops){
+		var xScale = d3.scale.linear().domain([0, w]).range([0, w]);
+		var yScale = d3.scale.linear().domain([h, 0]).range([h, 0]);
+		
 		var lineFunction = d3.svg.line()
-		                          .x(function(d) { return d.x; })
-		                          .y(function(d) { return d.y; })
+		                          .x(function(d) { return xScale(d.x); })
+		                          .y(function(d) { return yScale(d.y); })
 		                          .interpolate("linear");
 															
 		for(var i = 0; i < trains.length; i++){
@@ -62,8 +62,8 @@
 										});
 						}
 					}
+					//train coloring and drawing
 					if(trainPath.length > 1){
-						// for(var i= 0; i < train.time_stops; i++){console.log(train.time_stops[i])}
 						var color = "black";
 						if(train.train_type === 2){
 							color = "blue"
@@ -74,10 +74,8 @@
 						svg.append("path")
 							.attr("d", lineFunction(trainPath))
 							.attr("stroke", color)
-							.attr("stroke-width", 2)
+							.attr("stroke-width", 1)
 							.attr("fill", "none");
-							
-							// debugger;
 					}
 				}
 			})
@@ -86,43 +84,52 @@
 	}
 	
 	var buildGrid = function(svg, h, w, stops){		
+ 		var xScale = d3.scale.linear().domain([0, w]).range([0, w]);
+ 		var yScale = d3.scale.linear().domain([h, 0]).range([h, 0]);
+		var lineFunction = d3.svg.line()
+		                          .x(function(d) { return xScale(d.x); })
+		                          .y(function(d) { return yScale(d.y); })
+		                          .interpolate("linear");
+		
+		//draw stop names
 		svg.selectAll("text").data(stops).enter().append("text")
 			 .text(function(d){return d.name})
-			 .attr("x", 0)
-			 .attr("y", function(d){return d.id *  h/stops.length-0.2 * h/stops.length})
+			 .attr("x", xScale(10))
+			 .attr("y", function(d){
+				 return yScale(.99 * d.id *  h / stops.length) + 12.5
+			 })
        .attr("fill", "gray")
 			 .attr("stroke", "gray")
-			 .style("font-size", 12);
+			 .style("font-size", 10);
 			 
-		
-		var lineFunction = d3.svg.line()
-		                          .x(function(d) { return d.x; })
-		                          .y(function(d) { return d.y; })
-		                          .interpolate("linear");
+
+	 h/stops.length * i + yoffset
+
 															
 															
 		var xoffset = 100;
 		var yoffset = 20;
 		
-		//zones
+		//zones "data"
 		var zone_ids = [[0, 5, "#eee"], 
 										[5, 13, "#ccc"],
 										[13, 20, "#aaa"],
 										[20, 26, "#999"],
 										[26, 28, "#777"],
 										[28, 30, "#555"]];
-										
+		//draw zones								
 		svg.selectAll("rect").data(zone_ids).enter().append("rect")
-				.attr("x", that.xoffset)
-				.attr("y", function(d){return d[0] * h/stops.length + that.xoffset/5})
-				.attr("width", w - that.xoffset)
-				.attr("height", function(d){
-					return ((d[1] - d[0]) * h/stops.length)
+				.attr("x", xScale(that.xoffset))
+				.attr("y", function(d){
+					return yScale(d[0] * h / stops.length + that.xoffset / 5)
 				})
-				.attr("fill", function(d){ 
-					// debugger;
-					return d[2]});
-
+				.attr("width", xScale(w - that.xoffset))
+				.attr("height", function(d){
+					return yScale((d[1] - d[0]) * h/stops.length)
+				})
+				.attr("fill", function(d){ return d[2]});
+					
+		//horitzontal lines
 		for(var i = 0; i <= stops.length; i++){
 			var widthLine = [
 					{"x": xoffset, "y": h/stops.length * i + yoffset}, 
@@ -131,14 +138,14 @@
 			svg.append("path")
 				.attr("d", lineFunction(widthLine))
 				.attr("stroke", "black")
-				.attr("stroke-width", 2)
+				.attr("stroke-width", 1)
 				.attr("fill", "none");
 		}
-		
+		//vertical lines
 		for(var i = 0; i <= (21 * 6); i++){
 			var heightLine = [
 					{"x": xoffset + i * (w-xoffset)/ (21 * 6), "y": yoffset}, 
-					{"x": xoffset + i * (w-xoffset)/ (21 * 6), "y": h - yoffset}
+					{"x": xoffset + i * (w-xoffset)/ (21 * 6), "y": h * 1.1 - yoffset}
 				];
 			var color = "gray";
 			if(i % 6 === 0){
@@ -147,12 +154,13 @@
 				///6 to convert to hour, + 4 for offset, %12 to convert to am/pm
 				if((i/6) % 12 === 8){
 					if((i/6) % 24 === 8 ){
-						content = "noon"
+						content = "n"
 					} else {
-						content = "midnight"
+						content = "m"
 						
 					}
 				}
+				//draw time
 				svg.append("text")
 					 .attr({"x": xoffset + i * (w-xoffset)/ (21 * 6), "y": 15})
 					 .attr("fill", "gray")
